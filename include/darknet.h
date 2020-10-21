c6351eb60fd3975ae1ed433da17cf417f47912cf
Andrei Costinescu <andrei.costinescu@tum.de>
2020-10-21 11:24:12 +0200

    Add verbose flag to network_predict and include file...

--- include/darknet.h
+++ include/darknet.h
@@ -1007,7 +1007,7 @@ LIB_API void do_nms_obj(detection *dets, int total, int classes, float thresh);
 LIB_API void diounms_sort(detection *dets, int total, int classes, float thresh, NMS_KIND nms_kind, float beta1);
 
 // network.h
-LIB_API float *network_predict(network net, float *input);
+LIB_API float *network_predict(network net, float *input, bool verbose = false);
 LIB_API float *network_predict_ptr(network *net, float *input);
 LIB_API detection *get_network_boxes(network *net, int w, int h, float thresh, float hier, int *map, int relative, int *num, int letter);
 LIB_API det_num_pair* network_predict_batch(network *net, image im, int batch_size, int w, int h, float thresh, float hier, int *map, int relative, int letter);
