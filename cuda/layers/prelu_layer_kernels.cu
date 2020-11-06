f2831d6ce7314329981008040fd8b84921b55398
Andrei Costinescu <andrei.costinescu@yahoo.com>
2020-11-06 17:08:39 +0100

    Better/faster? prelu computation on cuda

--- cuda/layers/prelu_layer_kernels.cu
+++ cuda/layers/prelu_layer_kernels.cu
@@ -8,10 +8,9 @@ extern "C" {
 #include "dark_cuda.h"
 }
 
-# if 1
+# if 0
 
-__global__ void forward_prelu_layer_kernel(int n, int w, int h, int c, int g, float *input, float *weights,
-                                           float *output) {
+__global__ void forward_prelu_kernel(int n, int w, int h, int c, int g, float *input, float *weights, float *output) {
     int id = (blockIdx.x + blockIdx.y * gridDim.x) * blockDim.x + threadIdx.x;
     if (id >= n) return;  // all channel index
 
@@ -28,33 +27,42 @@ __global__ void forward_prelu_layer_kernel(int n, int w, int h, int c, int g, fl
 extern "C" void forward_prelu_layer_gpu(const layer l, network_state net) {
     size_t n = l.c * l.batch;
 
-    forward_prelu_layer_kernel<<<cuda_gridsize(n), BLOCK>>>(n, l.w, l.h, l.c, l.groups, net.input, l.weights_gpu,
-                                                            l.output_gpu);
+    forward_prelu_kernel<<<cuda_gridsize(n), BLOCK>>>(n, l.w, l.h, l.c, l.groups, net.input, l.weights_gpu,
+                                                      l.output_gpu);
     check_error(cudaPeekAtLastError());
 }
 #else
+
 __global__ void prelu_kernel(float *input, float *output, float *alpha, int c, int n, int size) {
     int offset = blockIdx.x * blockDim.x + threadIdx.x;  // pixel index
-    int filter = blockIdx.y;  // channel index
-    int batch = blockIdx.z;  // data index
-    int idx_n = filter / n;
-    int index = (batch*c+filter)*size + offset;
 
-    if(offset < size)
-        output[index] = input[index] < 0 ? input[index]*alpha[idx_n] : input[index];
+    if (offset < size) {
+        int filter = blockIdx.y;  // channel index
+        int batch = blockIdx.z;  // data index
+        int idx_n = filter / n;
+        int index = (batch * c + filter) * size + offset;
+        float input_val = input[index];
+        
+        /*
+        output[index] = input_val < 0 ? input_val * alpha[idx_n] : input_val;
+        /*/
+        int condition = input_val >= 0;
+        output[index] = input_val * (condition + alpha[idx_n] * (1 - condition));
+        //*/
+    }
 }
 
 extern "C" void prelu_gpu(float *input, float *output, float *alpha, int batch, int c, int n, int size) {
-    dim3 dimGrid((size-1)/BLOCK + 1, c, batch);
+    dim3 dimGrid((size - 1) / BLOCK + 1, c, batch);
     dim3 dimBlock(BLOCK, 1, 1);
 
     prelu_kernel<<<dimGrid, dimBlock>>>(input, output, alpha, c, n, size);
     check_error(cudaPeekAtLastError());
 }
 
-extern "C" void forward_prelu_layer_gpu(layer l, network net) {
-    fill_gpu(l.outputs*l.batch, 0, l.output_gpu, 1);
-    prelu_gpu(net.input_gpu, l.output_gpu, l.weights_gpu, l.batch, l.c, l.groups, l.w*l.h);
+extern "C" void forward_prelu_layer_gpu(const layer l, network_state net) {
+    fill_ongpu(l.outputs * l.batch, 0, l.output_gpu, 1);  // fill output_gpu with 0s
+    prelu_gpu(net.input, l.output_gpu, l.weights_gpu, l.batch, l.c, l.groups, l.w * l.h);
 }
 #endif
 
