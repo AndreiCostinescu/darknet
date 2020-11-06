09cffac993b33376653832274430c6714b90e924
Andrei Costinescu <andrei.costinescu@yahoo.com>
2020-11-06 09:05:08 +0100

    FIX BUG OF IDENTITY LAYER that the memory copying was too short (size * sizeof(dataType)!!!)

--- cuda/layers/identity_layer_kernels.cu
+++ cuda/layers/identity_layer_kernels.cu
@@ -4,14 +4,12 @@
 
 extern "C" {
 #include "layers/identity_layer.h"
-#include "utils/blas.h"
 #include "dark_cuda.h"
 }
 
 
 extern "C" void forward_identity_layer_gpu(const layer l, network_state net) {
-    cudaMemcpy(l.output_gpu, net.input, l.batch * l.inputs, cudaMemcpyDeviceToDevice);
-    check_error(cudaPeekAtLastError());
+    check_error(cudaMemcpy(l.output_gpu, net.input, l.batch * l.inputs * sizeof(float), cudaMemcpyDeviceToDevice));
 }
 
 extern "C" void backward_identity_layer_gpu(layer l, network_state net) {}
